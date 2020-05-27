41625495a26cccceb68279a3d9898a1a59e8c89b
FrankTN <franktn15@gmail.com>
2020-05-28 00:05:55 +0200

    Added a function which erodes the mask with a 20x20 square, will add random sampling

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
 import logging
+from random import randint
+
+import numpy as np
+from scipy import ndimage
 
 import radiomics
 from radiomics import featureextractor
+from radiomics import generalinfo
+
 import SimpleITK as sitk
 import os
 
+#import utilities
 
-def extract_features(names, extractor):
-    """ This function extracts features from a list of file and masks as specified by the names parameter
-        returns a feature vector
+
+def ROI_sampling(mask: sitk.Image) -> sitk.Image:
+    """
+    Clips the default mask using a standard area, overcoming the area dependence of certain first order measures such as
+    Shannon entropy.
+    """
+    # The original type is float64, we convert to int32
+    discretized_mask = sitk.Cast(mask, sitk.sitkInt32)
+    output = sitk.Image(mask.GetSize(), sitk.sitkInt32)
+
+    # For each slice in the mask we try to find a suitable region
+    for z in np.arange(discretized_mask.GetDepth()):
+        # Extract a slice from the mask
+        image = discretized_mask[:, :, z.item()]
+        img_arr = sitk.GetArrayFromImage(image)
+        # Erode the mask using a 20 x 20 box
+        img_arr = ndimage.binary_erosion(img_arr, structure=np.ones((20, 20))).astype(img_arr.dtype)
+
+        # Pick a random location within the eroded mask, this will be the new center of our window
+        indices = np.nonzero(img_arr)
+        # Check if there are any indices in the tuple
+        if len(indices[0]):
+            random_index = randint(0, len(indices[0])-1)
+            center = indices[0][random_index], indices[1][random_index]
+            # Create a new image of the intersect of the mask with the selected window
+
+        # Paste the new image into the output image
+        img_vol = sitk.JoinSeries(sitk.GetImageFromArray(img_arr))
+        output = sitk.Paste(output, img_vol, img_vol.GetSize(), destinationIndex=[0, 0, z.item()])
+    return output
+
+
+def extract_features(files, extractor):
+    """
+    This function extracts features from a list of file and masks as specified by the names parameter
+    returns a feature vector
     """
     featureVector = list()
     print("Calculating features")
-    for (imageName, maskName) in names:
-        featureVector.append(extractor.execute(imageName, maskName))
+    for (image, mask) in files:
+        featureVector.append(extractor.execute(image, mask))
     return featureVector
 
 
@@ -23,34 +63,11 @@ def print_features(features_list):
             print("Computed %s: %s" % (featureName, features[featureName]))
 
 
-# get pyradiomics logger, loglevel DEBUG
-logger = radiomics.logger
-logger.setLevel(logging.DEBUG)
-
-# Set up the handler to write out all log entries to a file
-handler = logging.FileHandler(filename='testLog.txt', mode='w')
-formatter = logging.Formatter("%(levelname)s:%(name)s: %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-# Define settings for signature calculation
-# These are currently set equal to the respective default values
-settings = {'binWidth': 25, 'resampledPixelSpacing': None, 'interpolator': sitk.sitkBSpline}
-print(settings)
-
-# Initialize feature extractor
-f_extractor = featureextractor.RadiomicsFeatureExtractor(**settings)
-
-# Disable all classes except firstorder
-f_extractor.disableAllFeatures()
-
-# Only enable mean and skewness in firstorder
-f_extractor.enableFeaturesByName(firstorder=['Mean', 'Skewness'])
-
+# Currently uses hardcoded paths
 imageName = os.path.realpath('data/tr_im.nii')
 maskName = os.path.realpath('data/tr_mask.nii')
 
-filenames = list()
-filenames.append((imageName, maskName))
-features = extract_features(filenames, f_extractor)
-print_features(features)
+image = sitk.ReadImage(imageName)
+mask = sitk.ReadImage(maskName)
+
+sitk.WriteImage(ROI_sampling(mask), 'sampled.nii')
